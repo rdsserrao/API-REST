0549bae2a14c99b3012190831584751cb431cc05
Ricardo Serrão <ricardoserrao_5@hotmail.com>
2021-01-26 18:43:52 +0000

    1st

---
+++ src/models/index.js
@@ -0,0 +1,32 @@
+let users = {
+    1: {
+    id: '1',
+    username: 'Robin Wieruch',
+  },
+  2: {
+    id: '2',
+    username: 'Dave Davids',
+  },
+  3: {
+    id: '3',
+    username: 'Ricardo Serrão',
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'Bye World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
